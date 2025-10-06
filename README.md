ecdbd0f48f64b0bbb956f4ffd6b91231395d5947
Jeremy Scheff <jdscheff@gmail.com>
2025-10-06 12:06:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# chrome-worker-stream-message
+
+To reproduce the issue:
+
+1. Go to https://dumbmatter.github.io/chrome-worker-stream-message/
+2. Select a large (>500MB) gzip file
+   (Running `head -c 500M /dev/random | base64 | gzip -c > test.gz` will create one)
+3. Once it starts decompressing, click "Cancel"
+4. Observe that in Chrome 140 the worker does not get the "cancel" message until the stream has is done processing, but in Firefox 143 it does get it during the stream and cancels it before it reaches 100%
+
+I'm not sure whether Chrome or Firefox is behaving correctly, but I suspect Firefox.
+
+If you add `await Promise.resolve();` inside the loop consuming the stream, that still doesn't fix it.
+
+But if you add `await new Promise(resolve => setTimeout(resolve));` then Chrome behaves like Firefox.
---
+++ worker.js
@@ -0,0 +1,46 @@
+let cancelRequested = false;
+
+const log = (message) => {
+    const formatted = `[worker] ${message}`;
+    postMessage(formatted);
+}
+
+onmessage = async e => {
+    if (e.data.file) {
+        log("got file");
+        cancelRequested = false;
+        processFile(e.data.file);
+    } else if (e.data.cancel) {
+        log("cancel message received");
+        cancelRequested = true;
+    }
+};
+
+const processFile = async (file) => {
+    log("starting decompression");
+    let readSize = 0;
+    const decompressedStream = file.stream().pipeThrough(
+		new TransformStream({
+			transform(chunk, controller) {
+				readSize += chunk.length;
+				controller.enqueue(chunk);
+			}
+		})
+	).pipeThrough(new DecompressionStream("gzip"));
+
+    for await (const value of decompressedStream) {
+        if (cancelRequested) {
+            log("cancelling stream");
+            break;
+        }
+
+        // This doesn't fix it:
+        // await Promise.resolve();
+
+        // This does fix it:
+        // await new Promise(resolve => setTimeout(resolve));
+    }
+
+    const percent = Math.round((readSize / file.size) * 100);
+    log(`finished (${percent}% done)`);
+}
